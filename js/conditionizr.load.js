e7ab1fcaae98d6ca8bd5eb0878cac4a65b6b4042
Dan Nguyen <daan.nguyen@gmail.com>
2013-04-04 21:44:30 +1100

    Initial commit of base templates and scss setup

---
+++ js/conditionizr.load.js
@@ -0,0 +1,12 @@
+conditionizr({
+  debug: false,
+  ieLessThan: {
+    active: true,
+    version: '9',
+    scripts: false,
+    styles: false,
+    classes: true,
+    // keep customScript on 1 line for IE
+    customScript: '//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.6.1/html5shiv.js, //cdnjs.cloudflare.com/ajax/libs/respond.js/1.1.0/respond.min.js'
+    }
+});
