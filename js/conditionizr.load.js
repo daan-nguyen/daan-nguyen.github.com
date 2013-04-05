e3c57e790a2057baf1665462d4382000943fab32
Dan Nguyen <daan.nguyen@gmail.com>
2013-04-05 16:02:19 +1100

    Playing around with responsive mixin to scale header.

--- js/conditionizr.load.js
+++ js/conditionizr.load.js
@@ -6,7 +6,7 @@ conditionizr({
     scripts: false,
     styles: false,
     classes: true,
-    // keep customScript on 1 line for IE
+    // keep customScript on 1 line for IE when not mini
     customScript: '//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.6.1/html5shiv.js, //cdnjs.cloudflare.com/ajax/libs/respond.js/1.1.0/respond.min.js'
-    }
+  }
 });
