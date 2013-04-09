5d7c4b80ee2171c69b79305f7e3762f6fa82973d
Dan Nguyen <daan.nguyen@gmail.com>
2013-04-09 20:54:31 +1000

    Added intro transition, modified header img and tweaks to type.

--- js/source/main.js
+++ js/source/main.js
@@ -1,23 +1,50 @@
 "use strict";
 
-(function() {
+(function(window) {
+  var my = window.dnSite = {};
+  var $win = $(window),
+      $headerImg = $('#main-header img');
 
-	$(document).ready(function() {
-		$('#main-header').css('opacity','1');
-	});
+  // bind fittext to header
+  $('#main-header h1').fitText(1.0, {minFontSize: '40px', maxFontSize: '180px'});
 
-	$(window).scroll(function() {
-		var winHeight = $(this).height(),
-			winScroll = $(this).scrollTop();
+  // banner img load loop
+  my.bannerLoader = function() {
+    if ($headerImg.get(0).complete) { //check for complete before starting transition
 
-		$('#main-content section').each(function() {
-			var t = $(this).offset().top;
+      setTimeout(function(){
+        $('#main-header').css('opacity','1');
+        if ($win.scrollTop() === 0) {
 
-			if (t < winHeight + winScroll) {
-				$(this).css('opacity','1');
-			}
-		});
-	});
+          $('html,body').animate({scrollTop: $headerImg.offset().top + $headerImg.height() - $win.innerHeight()}, 2000,
+            'swing', function() {
+              $('#main-header h1,h2').css('opacity', '1');
+            });
+        } else {
+          $('#main-header h1,h2').css('opacity', '1');
+        }
+      }, 300);
 
-	$('#main-header h1').fitText(1.0, {minFontSize: '40px', maxFontSize: '180px'});
-})();
+    } else { //delay, try again
+      setTimeout(my.bannerLoader, 100);
+    }
+  };
+
+  $win.scroll(function() {
+    var winHeight = $(this).height(),
+        winScroll = $(this).scrollTop();
+
+    $('#main-content section').each(function() {
+      var $t = $(this);
+
+      if ($t.offset().top < winHeight + winScroll) {
+        $t.css('opacity','1');
+      }
+    });
+  });
+
+})(window);
+
+$(document).ready(function() {
+  dnSite.bannerLoader();
+});
