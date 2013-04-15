7cfad9945751a6cf562b4d4df76991df1fc32af1
Dan Nguyen <daan.nguyen@gmail.com>
2013-04-15 17:54:55 +1000

    IE7/8 Compatiability, wording corrections, resume update, tweaks to transitions and google analytics added.

--- js/source/conditionizr.load.js
+++ js/source/conditionizr.load.js
@@ -1,10 +1,11 @@
 conditionizr({
   debug: false,
+  styleSrc: 'stylesheets/',
   ieLessThan: {
     active: true,
     version: '9',
     scripts: false,
-    styles: false,
+    styles: true,
     classes: true,
     // keep customScript on 1 line for IE when not mini
     customScript: '//cdnjs.cloudflare.com/ajax/libs/respond.js/1.1.0/respond.min.js'
