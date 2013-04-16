f5343d024fcea059fe3782c17836ca6d74e89a9c
Dan Nguyen <daan.nguyen@gmail.com>
2013-04-16 21:23:56 +1000

    IE6 compatiability, forced to fixed width. No respond poly.

--- js/source/conditionizr.load.js
+++ js/source/conditionizr.load.js
@@ -6,8 +6,10 @@ conditionizr({
     version: '9',
     scripts: false,
     styles: true,
-    classes: true,
+    classes: true
     // keep customScript on 1 line for IE when not mini
-    customScript: '//cdnjs.cloudflare.com/ajax/libs/respond.js/1.1.0/respond.min.js'
-  }
+    // customScript: '//cdnjs.cloudflare.com/ajax/libs/respond.js/1.1.0/respond.min.js'
+  },
+  ie8: { customScript: '//cdnjs.cloudflare.com/ajax/libs/respond.js/1.1.0/respond.min.js' },
+  ie7: { customScript: '//cdnjs.cloudflare.com/ajax/libs/respond.js/1.1.0/respond.min.js' }
 });
