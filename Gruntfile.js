e7b52c234d05bb50e9e81ddfab382b74d72fb89b
Dan Nguyen <daan.nguyen@gmail.com>
2014-01-30 08:28:28 +1100

    CSS build config setup.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,28 @@
 module.exports = function(grunt) {
 
+  // Config vars.
+  var config = {
+    build: {},
+    css: {},
+    js: {}
+  };
+
+  config.build.out = '_src/build/';
+
+  config.css.src = '_src/scss/',
+  config.css.out = 'css/',
+  config.css.copy = require('./' + config.css.src + 'copy.json'),
+  config.css.concat = require('./' + config.css.src + 'concat.json');
+
+  // config.js.path = '_src/js/',
+  // config.js.copy =
+  // config.js.concat =
+
   // Project config.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
 
+    // SASS
     sass: {
       dist: {
         options: {
@@ -12,18 +31,60 @@ module.exports = function(grunt) {
         },
         files: [{
           expand: true,
-          src: ['_src/scss/*.scss'],
-          dest: 'css',
+          src: [config.css.src + '*.scss'],
+          dest: config.build.out,
           ext: '.css',
           flatten: true
         }]
       }
+    },
+
+    // Copy tasks
+    copy: {
+      css: {
+        files: [
+          { expand: true,
+            src: config.css.copy,
+            dest: config.build.out,
+            flatten: true
+          }
+        ]
+      }
+    },
+
+    // Concat tasks
+    concat: {
+      css: {
+        src: config.css.concat.map(function(file) { return config.build.out + file; }),
+        dest: config.css.out + 'main.css'
+      }
     }
+
   });
 
+  // Load plugins
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+
+
+  // Cleaning task
+  grunt.registerTask('clean', 'Clean task.', function() {
+
+    // Clean build dir
+    grunt.file.expand(config.build.out + '*').forEach(function(file) {
+      grunt.file.delete(file);
+    });
+    // Clean css dir
+    grunt.file.expand(config.css.out + '*').forEach(function(file) {
+      grunt.file.delete(file);
+    });
+
+  });
+
+  // Tasks list
+  grunt.registerTask('build', ['sass', 'copy', 'concat']);
 };
