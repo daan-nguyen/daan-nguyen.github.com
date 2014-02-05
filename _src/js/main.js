a396737e54c369d8ff26547ce42338c08c138567
Dan Nguyen <daan.nguyen@gmail.com>
2014-02-05 17:28:08 +1100

    Mobile menu in pretty good state, smooth 60fps on iphone4s. Removed jquer from deps list.

--- _src/js/main.js
+++ _src/js/main.js
@@ -1,10 +1,14 @@
 (function() {
 	'use strict';
 
-  $('header').click(function() {
-    $(this).toggleClass('show');
-  });
+  // mobile nav button
+  var openNavEl = document.getElementsByClassName('nav-open')[0];
+  var headerEl = document.getElementsByTagName('header')[0];
+  openNavEl.addEventListener('click', function(event) {
+    headerEl.classList.toggle('show');
+  }, false);
 
+  // bind fastclick
   FastClick.attach(document.body);
 
 })();
